f796d14f633d53ec4e4a34f5258d19260654b37b
Sushim Mukul Dutta <sushimmdutta@gmail.com>
2018-03-19 16:51:09 +0530

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # heroku-croner
-croner
+
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+const cmd = require('node-cmd');
+class script {
+    static runThis() {
+        console.log(`Running script`);
+        return cmd.get(`npm install in-memory-queue
+        npm install fs.promiser
+        npm install fs.proms
+        npm i promise-request-retry`, function (err, data) {
+            if (!err) {
+                console.log('Installation done :\n\n', data);
+                // process.exit(0);
+            } else {
+                console.log('error', err);
+                // process.exit(1);
+            }
+        })
+    }
+}
+
+module.exports = script.runThis();
